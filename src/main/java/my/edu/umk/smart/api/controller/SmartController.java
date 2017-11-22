a4c300edcfc114ecc1fe4ce530dc58f45b8e344e
hanifnoordin <hanifnoordin@umk.edu.my>
2017-11-22 12:02:38 +0800

    testing for smart api radius manager

--- src/main/java/my/edu/umk/smart/api/controller/SmartController.java
+++ src/main/java/my/edu/umk/smart/api/controller/SmartController.java
@@ -12,13 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import my.edu.umk.pams.connector.payload.CandidatePayload;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-
-import my.edu.umk.pams.connector.payload.StudentPayload;
 
 /**
  */
@@ -29,9 +28,12 @@ public class SmartController {
 
     @Autowired
     private JdbcTemplate oraJdbcTemplate;
+    
+    @Autowired
+    private JdbcTemplate mysqlJdbcTemplate;
 
     @RequestMapping(value = "/student/smartCardPelajar", method = RequestMethod.POST)
-    public ResponseEntity<String> smartCardPelajar(@RequestBody StudentPayload payload) {
+    public ResponseEntity<String> smartCardPelajar(@RequestBody CandidatePayload payload) {
 
         final String sql = "insert into smartcard_pelajar(matricNo,name) values(?,?)";
 
@@ -48,7 +50,41 @@ public class SmartController {
     }
     
     @RequestMapping(value = "/student/smartCardEncodePelajar", method = RequestMethod.POST)
-    public ResponseEntity<String> smartCardEncodePelajar(@RequestBody StudentPayload payload) {
+    public ResponseEntity<String> smartCardEncodePelajar(@RequestBody CandidatePayload payload) {
+
+        final String sql = "insert into smartcard_encode_pelajar(matricNo,name) values(?,?)";
+
+        oraJdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setString(1, payload.getMatricNo());
+                ps.setString(2, payload.getName());
+                return ps;
+            }
+        });
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/student/radiusManager/rmUser", method = RequestMethod.POST)
+    public ResponseEntity<String> radiusUser(@RequestBody CandidatePayload payload) {
+
+        final String sql = "insert into smartcard_encode_pelajar(matricNo,name) values(?,?)";
+
+        oraJdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setString(1, payload.getMatricNo());
+                ps.setString(2, payload.getName());
+                return ps;
+            }
+        });
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/student/radiusManager/radCheck", method = RequestMethod.POST)
+    public ResponseEntity<String> radCheckUser(@RequestBody CandidatePayload payload) {
 
         final String sql = "insert into smartcard_encode_pelajar(matricNo,name) values(?,?)";
 
