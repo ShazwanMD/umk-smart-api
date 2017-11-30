90b54bf48b00e60843642f4afbdb24d176cbc87d
hanifnoordin <hanifnoordin@umk.edu.my>
2017-11-30 13:01:16 +0800

    completed smart api. ready for production

--- src/main/java/my/edu/umk/smart/config/ApplicationConfig.java
+++ src/main/java/my/edu/umk/smart/config/ApplicationConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @ComponentScan(basePackages = {
